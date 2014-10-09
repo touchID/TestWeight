943f53629cba004fafbeb2c5503f3eef581e0521
iOS魔术师 <ios.7@qq.com>
2014-10-09 20:42:00 +0800

    1

--- TestWeight.playground/section-1.swift
+++ TestWeight.playground/section-1.swift
@@ -3,3 +3,26 @@
 import Cocoa
 
 var str = "Hello, playground"
+
+    int height
+    
+/*
+* 输入身高，计算标准体重
+* 成年:〔身高(cm)-100〕×0.9=标准体重(kg)
+* @2014，touchID
+*/
+
+//#include <stdio.h>
+
+//int main()
+//{
+
+//    printf("请输入成人身高(>100):\n");
+//    scanf("%d",&height);
+//    if(height<=100){
+//        printf("输入的身高错误\n");
+//        return 1;
+//    }
+//    printf("当身高为:%d(cm),理论标准体重为%f(kg)\n",height,(height-100)*0.9);
+//    return 0;
+//}
